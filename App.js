e90f08fee8c5b7102ce15ae7e55db84be9b9437d
udant <61499772+udant@users.noreply.github.com>
2020-07-12 14:34:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import ReadScreen from './ReadScreen'
+import WriteScreen from './WriteScreen'
+export default function App() {
+  return (
+    /*<View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>*/
+    <AppContainer />
+  );
+}
+
+const TabNavigator = createBottomTabNavigator({
+  Read:{screen:ReadScreen},
+  Write:{screen:WriteScreen},
+});
+
+const AppContainer = createAppContainer(TabNavigator);
+
+const styles = StyleSheet.create({
+ /* container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },*/
+});
---
+++ WriteScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text,View} from 'react-native';
+
+export default class WriteScreen extends React.Component  {
+    render(){
+        return(
+            <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
+                <Text>
+                   Read A Story 
+                </Text>
+            </View>
+        );
+    }
+}
